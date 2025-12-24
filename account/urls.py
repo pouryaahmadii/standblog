8df3f6a8ae74208836e44c4f9f420e677c80f3c1
Pourya Ahmadi <pouryaaahmadii@gmail.com>
2025-12-24 23:48:30 +0330

    stand blog

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-    path('login', views.user_login, name='login'),
+    path('login', views.UserLogin.as_view(), name='login'),
     path('logout', views.user_logout, name='logout'),
     path('register', views.user_register, name='register'),
     path('edit', views.user_edit, name='edit'),
--- account/views.py
+++ account/views.py
@@ -1,23 +1,28 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 from .forms import UserLoginForm, UserEditForm, RegisterForm
 
 
-def user_login(request):
-    if request.user.is_authenticated:
-        return redirect('home_app:main')
+class UserLogin(FormView):
+    template_name = 'account/user_login.html'
+    form_class = UserLoginForm
+    success_url = reverse_lazy('home_app:main')
 
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            user = User.objects.get(username=form.cleaned_data.get('username'))
-            login(request, user)
-            return redirect('home_app:main')
-    else:
-        form = UserLoginForm()
-    return render(request, 'account/user_login.html', {'form': form})
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.success(request, "You are already logged in")
+            return redirect(self.get_success_url())
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        user = authenticate(username=username, password=form.cleaned_data.get('password'))
+        login(self.request, user)
+        messages.success(self.request, "You are successfully logged in")
+        return super().form_valid(form)
 
 
 def user_register(request):
